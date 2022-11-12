f1ff4fe2b002bf965e74c300b2798377a784ec64
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2022-11-13 00:17:18 +0100

    prototype

---
+++ main.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// Handle requests for static files
+app.use(express.static("www"));
+
+// Start the server
+server.listen('3000', () => {
+    console.log('Local DevServer Started on port 3000...');
+});
+
+const WebSocket = require('ws');
+const wsServer = new WebSocket.Server({
+    server: server
+});
+
+let sockets = {};
+wsServer.on('connection', function(socket) {  
+    // When you receive a message, send that message to every socket.
+    socket.on('message', function(buffer) {
+        const data = JSON.parse(buffer.toString());
+        console.log("Message", data);
+        switch (data.type) {
+            case "login":
+                if (sockets[data.channel]) {
+                    sockets[data.channel].push(socket);
+                }
+                else {
+                    sockets[data.channel] = [ socket ];
+                }
+            break;
+
+            default:
+                throw new RangeError(`${data.type} not found`);
+            // break;
+        }
+        sockets[data.channel].forEach(s => s.send(buffer));
+    });
+  
+    // When a socket closes, or disconnects, remove it from the array.
+    socket.on('close', function() {
+        for (const channel in sockets) {
+            if (Object.hasOwnProperty.call(sockets, channel)) {
+                sockets[channel] = sockets[channel].filter(s => s !== socket);   
+            }
+        }
+    });
+
+    socket.on('open', function() {
+        console.log("WebSocket open");
+    });
+});
+console.log("Local WebSocket Server Started on port 3000...");
---
+++ www/script.js
@@ -0,0 +1,69 @@
+const socket = new WebSocket('ws://localhost:3000/');
+
+//handle messages from the server 
+socket.onmessage = async function (message) { 
+	var data = JSON.parse(await message.data.text());
+	console.log("Got message", data);
+	
+//    switch(data.type) { 
+//       case "login": 
+//          onLogin(data.success); 
+//          break; 
+//       case "offer": 
+//          onOffer(data.offer, data.name); 
+//          break; 
+//       case "answer": 
+//          onAnswer(data.answer); 
+//          break; 
+//       case "candidate": 
+//          onCandidate(data.candidate); 
+//          break; 
+//       default: 
+//          break; 
+//    } 
+};
+  
+//when a user logs in 
+// function onLogin(success) { 
+
+//    if (success === false) { 
+//       alert("oops...try a different username"); 
+//    } else { 
+//       //creating our RTCPeerConnection object 
+		
+//       var configuration = { 
+//          "iceServers": [{ "url": "stun:stun.1.google.com:19302" }] 
+//       }; 
+		
+//       myConnection = new webkitRTCPeerConnection(configuration); 
+//       console.log("RTCPeerConnection object was created"); 
+//       console.log(myConnection); 
+  
+//       //setup ice handling
+//       //when the browser finds an ice candidate we send it to another peer 
+//       myConnection.onicecandidate = function (event) { 
+		
+//          if (event.candidate) { 
+//             send({ 
+//                type: "candidate", 
+//                candidate: event.candidate 
+//             }); 
+//          } 
+//       }; 
+//    } 
+// };
+  
+socket.onopen = function () { 
+   console.log("Connected"); 
+};
+  
+socket.onerror = function (err) { 
+   console.log("Got error", err); 
+};
+
+socket.onopen = () => {
+	socket.send(JSON.stringify({
+		type: "login",
+		channel: "testChannel"
+	}));
+};
