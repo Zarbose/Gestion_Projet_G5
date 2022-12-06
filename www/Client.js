d790ef4f2283f855af947c4f5128c73031ecd060
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2022-12-06 21:54:50 +0100

    client refactor and audio support

---
+++ www/Client.js
@@ -0,0 +1,179 @@
+export class WssClient {
+	#socket = WebSocket;
+	#iceClient = IceClient;
+	channel = "";
+
+	/**
+	 * @param {IceClient} iceClient 
+	 * @param {string} [ip] server
+	 * @param {int} [port] server
+	 */
+	constructor(iceClient, ip = "localhost", port = 8443) {
+		const url = new URL(`wss://${ip}:${port}/`);
+		this.#socket = new WebSocket(url);
+		this.#iceClient = iceClient;
+	}
+
+	/**
+	 * @description WebSocket.send a String from a JSON
+	 * @param {Object} json 
+	 */
+	sendJSON(json) {
+		this.#socket.send(
+			JSON.stringify(
+				json
+			)	
+		);
+	}
+
+	/**
+	 * @description WebSocket.send
+	 * @param {string} json 
+	 */
+	send(string) {
+		this.#socket.send(string);
+	}
+
+	/**
+	 * @param {function(data): void} login Frontend function to be called when connected
+	 */
+	start(login) {
+		const globalThis = this;
+		this.#socket.onmessage = function (message) {
+			message.data.text().then(string => {
+				const data = JSON.parse(string);
+			
+				console.info("Socket received", data.type);
+		
+				switch(data.type) {
+				case "message":
+					console.info(`Channel ${data.type}: ${data.message}`);
+					break;
+				case "RTCPeerOffer":
+					// console.log(`Channel ${data.type}: RTCPeerOffer`, new RTCSessionDescription(data.offer));
+					globalThis.#iceClient.peerOffer(data);
+					break;
+				case "RTCPeerAnswer": {
+					globalThis.#iceClient.peerAnswer(data);
+					break;
+				}
+				case "IceCandidate": {
+					globalThis.#iceClient.newCandidate(data);
+					break;
+				}
+				case "login":
+					login(data);
+					break;
+				case "error":
+					console.error(data.msg);
+					break;
+				default:
+					throw new Error(`Internal error, ${data.type} no action to be done`);
+					// break;
+				}
+			});
+		};
+		this.#socket.onerror = function (err) { 
+			console.log("Got error", err); 
+		};
+		this.#socket.onopen = function () { 
+			console.log("Connected");
+		};
+	}
+
+}
+
+export class IceClient {
+	#rtcPeerConnection = new RTCPeerConnection();
+	#wssClient = WssClient;
+	channel = "";
+
+	/**
+	 * @param {string} channel 
+	 */
+	constructor(channel) {
+		this.channel = channel;
+	}
+
+	/**
+	 * @param {WssClient} wssClient 
+	 * @param {function(MediaStreamTrack[]): void} videoTrack Frontend function to be called when new video stream arrives
+	 */
+	start(wssClient, videoTrack) {
+		this.#wssClient = wssClient;
+		this.#rtcPeerConnection.addEventListener("icecandidate", (event) => {
+			console.info("New IceCandidate", event.candidate);
+			if (event.candidate) {
+				this.#wssClient.sendJSON({
+					type: "IceCandidate",
+					channel: this.channel,
+					candidate: event.candidate.toJSON()
+				});
+			}
+		});
+		this.#rtcPeerConnection.ontrack = (event) => {
+			if (event.track.kind === "audio") {
+				console.log("Received audio MediaStreamTrack");
+			}
+			else {
+				videoTrack(event.streams);
+			}
+		};
+	}
+
+	/**
+	 * @param {MediaStream} srcObject 
+	 */
+	sendVideo(srcObject) {
+		this.#rtcPeerConnection.addTrack(srcObject.getVideoTracks()[0], srcObject);
+		console.log(srcObject.getTracks());
+		this.#rtcPeerConnection.addTrack(srcObject.getAudioTracks()[0], srcObject);
+
+		this.#rtcPeerConnection.createOffer().then(offer => 
+			this.#rtcPeerConnection.setLocalDescription(offer)
+		).then(() => {
+			this.#wssClient.sendJSON({
+				type: "RTCPeerOffer",
+				channel: this.channel,
+				offer: this.#rtcPeerConnection.localDescription.toJSON()
+			});
+		});
+
+	}
+
+	peerOffer(data) {
+		this.#rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(data.offer)).then(() => {
+			this.#rtcPeerConnection.createAnswer().then(answer => {
+				this.#rtcPeerConnection.setLocalDescription(answer).then(() => {
+					this.#wssClient.sendJSON({
+						type: "RTCPeerAnswer",
+						channel: this.channel,
+						answer: this.#rtcPeerConnection.localDescription.toJSON()
+					});
+				});
+			});
+		}).catch(error =>
+			console.error(error, new RTCSessionDescription(data.offer))
+		);
+	}
+
+	peerAnswer(data) {
+		this.#rtcPeerConnection.setRemoteDescription(
+			new RTCSessionDescription(data.answer)
+		).then(() => {
+			console.info("RTCPeerConnection established");
+		}).catch(error =>
+			console.error(error, new RTCSessionDescription(data.answer))
+		);
+	}
+
+	newCandidate(data) {
+		this.#rtcPeerConnection.addIceCandidate(
+			new RTCIceCandidate(data.candidate)
+		).then(
+			console.log("IceCandidate added")
+		).catch(error =>
+			console.error(error, new RTCIceCandidate(data.candidate), this.#rtcPeerConnection.localDescription)
+		);
+	}
+}
--- www/script.js
+++ www/script.js
@@ -1,139 +1,37 @@
-fetch(`${window.location.origin}/API?channels`).then(response => {
-	const input = document.getElementById("existingChannels");
-	response.json().then(json => {
-		if (json.length != 0) {
-			for (const channel of json) {
-				input.insertAdjacentHTML("afterbegin", `<option value="${channel}">${channel}</option>`);
-			}
-		}
-		else {
-			input.disabled = true;
-		}
-	});
-});
+import { IceClient, WssClient } from "./Client.js";
 
-const socket = new WebSocket("wss://localhost:8443/");
 let channel = "";
-
-socket.onmessage = function (message) {
-	message.data.text().then(string => {
-		const data = JSON.parse(string);
-	
-		console.info("Socket received", data.type);
-
-		switch(data.type) {
-		case "message":
-			console.info(`Channel ${data.type}: ${data.message}`);
-			break;
-		case "RTCPeerOffer": {
-			// console.log(`Channel ${data.type}: RTCPeerOffer`, new RTCSessionDescription(data.offer));
-
-			localConnection.setRemoteDescription(new RTCSessionDescription(data.offer)).then(() => {
-				localConnection.createAnswer().then(answer => {
-					localConnection.setLocalDescription(answer).then(() => {
-						socket.send(JSON.stringify({
-							type: "RTCPeerAnswer",
-							channel: channel,
-							answer: localConnection.localDescription.toJSON()
-						}));
-					});
-				});
-			}).catch(error =>
-				console.error(error, new RTCSessionDescription(data.offer))
-			);
-			break;
+let login = (data) => {
+	if (data.state === "success") {
+		let form  = document.getElementById("choose");
+		for (const element of form.elements) {
+			element.disabled = true;
 		}
-		case "RTCPeerAnswer": {
-			localConnection.setRemoteDescription(new RTCSessionDescription(data.answer)).then(() => {
-				console.info("RTCPeerConnection established");
-			}).catch(error =>
-				console.error(error, new RTCSessionDescription(data.answer))
-			);
-			break;
-		}
-		case "IceCandidate": {
-			localConnection.addIceCandidate(new RTCIceCandidate(data.candidate)).then(
-				console.log("IceCandidate added")
-			).catch(error =>
-				console.error(error, new RTCIceCandidate(data.candidate), localConnection.localDescription)
-			);
-			break;
-		}
-		case "login":
-			if (data.state === "success") {
-				let form  = document.getElementById("choose");
-				for (const element of form.elements) {
-					element.disabled = true;
-				}
-			}
-			else {
-				const input = document.querySelector("form#choose input");
-				input.setCustomValidity("Erreur lors de l'enregitrement de channel !");
-				input.reportValidity();
-			}
-			break;
-		case "error":
-			console.error(data.msg);
-			break;
-		default:
-			throw new Error(`Internal error, ${data.type} no action to be done`);
-			// break;
-		}
-	});
-};
-
-
-const localConnection = new RTCPeerConnection();
-localConnection.addEventListener("icecandidate", (event) => {
-	console.info("New IceCandidate", event.candidate);
-	if (event.candidate) {
-		socket.send(JSON.stringify({
-			type: "IceCandidate",
-			channel: channel,
-			candidate: event.candidate.toJSON()
-		}));
 	}
-});
-
-localConnection.ontrack = (event) => {
+	else {
+		const input = document.querySelector("form#choose input");
+		input.setCustomValidity("Erreur lors de l'enregitrement de channel !");
+		input.reportValidity();
+	}
+};
+let videoTrack = (streams) => {
+	if (streams.length <= 0) throw new Error("Streams are empty !");
 	const videoChannel = document.getElementById("videoChannel");
 	const video = document.createElement("video");
 	video.muted = false;
 	video.controls = false;
 	video.autoplay = true;
-	video.srcObject = event.streams[0];
+	video.srcObject = streams[0];
 	video.width = 400;
 	video.height = 200;
 	videoChannel.appendChild(video);
 	console.info("New stream added in DOM");
 };
 
-socket.onerror = function (err) { 
-	console.log("Got error", err); 
-};
-socket.onopen = function () { 
-	console.log("Connected");
-};
-
-document.getElementById("choose").addEventListener("submit", () => {
-	const existingChannels = document.getElementById("existingChannels").value;
-	const newChannel = document.getElementById("newChannel").value;
-	channel = !existingChannels ? newChannel : existingChannels;
-	socket.send(JSON.stringify({
-		type: "login",
-		channel: channel
-	}));
-});
-
-document.getElementById("send").addEventListener("submit", () => {
-	const message = document.getElementById("message").value;
-	socket.send(JSON.stringify({
-		type: "message",
-		channel: channel,
-		message: message
-	}));
-});
-
+const iceClient = new IceClient(channel);
+const wssClient = new WssClient(iceClient);
+iceClient.start(wssClient, videoTrack);
+wssClient.start(login);
 
 navigator.mediaDevices.getUserMedia({
 	video: {
@@ -161,15 +59,40 @@ navigator.mediaDevices.getUserMedia({
 
 document.getElementById("sendVideo").addEventListener("submit", () => {
 	const video = document.getElementById("webcam");
-	localConnection.addTrack(video.srcObject.getVideoTracks()[0], video.srcObject);
+	iceClient.sendVideo(video.srcObject);
+});
 
-	localConnection.createOffer().then(offer => 
-		localConnection.setLocalDescription(offer)
-	).then(() => {
-		socket.send(JSON.stringify({
-			type: "RTCPeerOffer",
-			channel: channel,
-			offer: localConnection.localDescription.toJSON()
-		}));
+document.getElementById("send").addEventListener("submit", () => {
+	const message = document.getElementById("message").value;
+	wssClient.sendJSON({
+		type: "message",
+		channel: channel,
+		message: message
+	});
+});
+
+document.getElementById("choose").addEventListener("submit", () => {
+	const existingChannels = document.getElementById("existingChannels").value;
+	const newChannel = document.getElementById("newChannel").value;
+	channel = !existingChannels ? newChannel : existingChannels;
+	wssClient.sendJSON({
+		type: "login",
+		channel: channel
+	});
+	iceClient.channel = channel;
+	wssClient.channel = channel;
+});
+
+fetch(`${window.location.origin}/API?channels`).then(response => {
+	const input = document.getElementById("existingChannels");
+	response.json().then(json => {
+		if (json.length != 0) {
+			for (const distantChannel of json) {
+				input.insertAdjacentHTML("afterbegin", `<option value="${distantChannel}">${distantChannel}</option>`);
+			}
+		}
+		else {
+			input.disabled = true;
+		}
 	});
 });
