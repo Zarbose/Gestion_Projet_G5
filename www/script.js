f7c146947df91aaaecf26b10e06d82525c6e0501
Zarbose <simonpieto.pieto@gmail.com>
2022-12-21 17:37:13 +0100

    update chat

--- www/script.js
+++ www/script.js
@@ -34,7 +34,7 @@ const login = (state) => {
 	}
 };
 const chat = (newUser, message) => {
-	document.getElementById("chatText").insertAdjacentHTML("afterbegin", `<span>${newUser}: ${message}</span>`);
+	document.getElementById("chatText").insertAdjacentHTML("afterbegin", `<div class="message">${newUser}: ${message}</div>`);
 };
 const videoTrack = (streams) => {
 	if (streams.length <= 0) throw new Error("Streams are empty !");
